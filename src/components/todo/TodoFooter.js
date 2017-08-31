5deb024ef5308a27b0c80e94e469dd8bd421443c
Ram Iyer <ram.iyer@bydeluxe.com>
2017-08-31 01:14:29 -0500

    broke footer into functions

--- src/components/todo/TodoFooter.js
+++ src/components/todo/TodoFooter.js
@@ -2,22 +2,28 @@
 import React from 'react'
 import {Link} from '../router'
 
+const todoCount = () => {
+  return (
+    <span className="todo-count"><strong>0</strong> item left</span>
+  )
+}
+
+const clearCompleted = () => {
+  return (
+    <button className="clear-completed">Clear completed</button>
+  )
+}
+
 export const TodoFooter = () => {
   return (
     <footer className="footer">
-      <span className="todo-count"><strong>0</strong> item left</span>
+      { todoCount() }
       <ul className="filters">
-        <li>
-          <Link to='/'>All</Link>
-        </li>
-        <li>
-          <Link to='/active'>Active</Link>
-        </li>
-        <li>
-          <Link to='/complete'>Complete</Link>
-        </li>
+        <li><Link to='/'>All</Link></li>
+        <li><Link to='/active'>Active</Link></li>
+        <li><Link to='/complete'>Complete</Link></li>
       </ul>
-      <button className="clear-completed">Clear completed</button>
+      { clearCompleted() }
     </footer>
   )
 }
