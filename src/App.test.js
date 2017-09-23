d4e16735befe3229b9060991a61d20697cd069c0
Ram Iyer <ram.iyer@bydeluxe.com>
2017-09-23 00:21:29 -0700

    adding jest-mock and mocking http request

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import fetchMock from 'fetch-mock'
+
+const mockResult = [
+  { "id": 1, "name": "Setup Server", "isComplete": false },
+  { "id": 2, "name": "Start Server", "isComplete": false }
+]
 
 describe('App', () => {
-  it('should be able to run tests', () => {
-      expect(1 + 2).toEqual(3);
+  it('renders without crashing', () => {
+    fetchMock.getOnce('http://localhost:3001/todos', mockResult)
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
   });
 });
